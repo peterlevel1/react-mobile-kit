6603fe7dcba0373b646970abd4c8c843e0cde53a
tina <3310156392@qq.com>
2021-10-04 06:41:40 +0800

    add

--- packages/h5/jest.config.js
+++ packages/h5/jest.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  modulePathIgnorePatterns: [],
-  moduleDirectories: ['node_modules', 'src/tests'],
+  // preset: '',
+  // 自定义转换形式，转换jsx、tsx文件
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest/dist',
+    '\\.jsx?$': 'babel-jest'
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx'],
+  modulePathIgnorePatterns: ['\/node_modules\/'],
+  moduleDirectories: ['node_modules', 'src/tests'],
+  // 在每个测试文件执行之前，运行一些代码以配置或设置测试框架。
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
+  globals: {},
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/src/tests/mocks/style-mock.js',
   },
